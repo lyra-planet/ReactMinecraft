2c13aff9fa4920dda5c99e52e247e0139ce8ceae
lyra-planet <1170158548@qq.com>
2022-10-22 20:21:23 +0800

    0.01

---
+++ src/images/image.js
@@ -0,0 +1,15 @@
+import dirtImg from './dirt.png'
+import grassImg from './grass.png'
+import glassImg from './glass.png'
+import logImg from './log.png'
+import woodImg from './wood.png'
+
+const images = {
+    dirtImg, 
+    grassImg,
+    glassImg,
+    logImg, 
+    woodImg, 
+}
+
+export default images
